7e76445a34429353a93994e71ef595a9d567722b
sinkhaha <1468709106@qq.com>
2021-07-19 15:25:26 +0800

    Create server.js

---
+++ helloWorldDemo/server.js
@@ -0,0 +1,35 @@
+/**
+ * grpc服务端 参考 https://grpc.io/docs/languages/node/quickstart/
+ */
+const grpc = require('@grpc/grpc-js');
+
+// 加载proto
+const hello_proto = require('./proto_loader');
+
+/**
+ *
+ * 实际的sayHello方法
+ * 
+ * @param {*} call
+ * @param {*} callback
+ */
+function sayHello(call, callback) {
+    console.log('客户端请求参数是', call.request);
+    // callback第2个参数为响应的消息
+    callback(null, { message: `hello, ${call.request.name}` });
+}
+
+function main() {
+    let server = new grpc.Server();
+
+    // 添加服务
+    server.addService(hello_proto.Greeter.service, { sayHello: sayHello });
+
+    // 启动服务
+    server.bindAsync('127.0.0.1:50051', grpc.ServerCredentials.createInsecure(), () => {
+        server.start();
+        console.log('grpc server started in 50051');
+    });
+}
+
+main();
